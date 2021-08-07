eb5060d6e5354e60ff396b14996e0459f35f2cd1
eduprov <admin@eduprov.com>
2021-08-07 16:08:15 +0530

    Eduprov : 07-08-2021 : work from botany

---
+++ app.js
@@ -0,0 +1,24 @@
+/*Navbar JS  */
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
+
+/* Testinomial slider */
+
+$(document).ready(function(){
+  $("#testimonial-slider").owlCarousel({
+      items:1,
+      itemsDesktop:[1000,1],
+      itemsDesktopSmall:[979,1],
+      itemsTablet:[768,1],
+      pagination:false,
+      navigation:true,
+      navigationText:["",""],
+      autoPlay:true
+  });
+});
